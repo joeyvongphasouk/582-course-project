ac046e4d69fa2e4cba5e183094a8f47140be002f
joeyvongphasouk <joey.vongpha@gmail.com>
2025-04-20 23:45:24 -0600

    intermed push, turbostat doesn't like wsl

--- CPU/cpu_test.py
+++ CPU/cpu_test.py
@@ -9,9 +9,10 @@ def main():
         print("\t cpu_test.py <ere|pcre> <input_search_file> <input_regex_file>")
         sys.exit(-1)
 
+    # execute correct func depending on opt
     if sys.argv[1] == "ere":
         ere_grep()
-    if sys.argv[1] == "ere_s":
+    elif sys.argv[1] == "ere_s":
         ere_grep_single_match()
     elif sys.argv[1] == "pcre":
         pcre_grep()
@@ -21,28 +22,27 @@ def main():
         print("incorrect usage, arg 1 should be either 'ere' or 'pcre'")
 
 
+# this function reads in a list of regexes performs egrep using each regex
 def ere_grep():
     with open(sys.argv[3], 'r') as rf, open('results/results.txt', 'w') as results:
         for line in rf:
             regex = line.strip()
-            results.write(f"----------- regex: {regex} \n")
-
             try:
-                output = subprocess.check_output(['egrep', '-x', regex, sys.argv[2]], text=True)
+                output = subprocess.check_output(['egrep', regex, sys.argv[2]], text=True)
                 results.write(output)
             except subprocess.CalledProcessError as e:
                 if e.returncode == 1:
-                    results.write("no matches found\n")
+                    results.write("no matches found\n") # note, this is similar to bkase's grep output
                 else:
                     results.write(f"error: {e} \n")
             results.write("\n")
 
+# this function takes in a string to perform egrep onto an input file
 def ere_grep_single_match():
     with open('results/results_single.txt', 'w') as results:
         regex = sys.argv[3].strip()
-        # results.write(f"----------- regex: {regex} \n")
         try:
-            output = subprocess.check_output(['egrep', '-x', regex, sys.argv[2]], text=True)
+            output = subprocess.check_output(['egrep', regex, sys.argv[2]], text=True)
             results.write(output)
         except subprocess.CalledProcessError as e:
             if e.returncode == 1:
