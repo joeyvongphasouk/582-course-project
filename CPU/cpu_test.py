9ddd91a76f3520b9df530cf5b1e7f8b0037dd51e
joeyvongphasouk <joey.vongpha@gmail.com>
2025-04-21 19:21:26 -0600

    intermed commit from linux to windows dev machine, revamping cpu impl

--- CPU/cpu_test.py
+++ CPU/cpu_test.py
@@ -2,11 +2,13 @@
 
 import sys
 import subprocess
+import time
+import psutil
 
 def main():
     if len(sys.argv) < 2:
         print("incorrect usage, arg 1 should be either 'ere' or 'pcre'")
-        print("\t cpu_test.py <ere|pcre> <input_search_file> <input_regex_file>")
+        print("\t cpu_test.py <ere|turbo> <input_search_file> <input_regex_file>")
         sys.exit(-1)
 
     # execute correct func depending on opt
@@ -14,28 +16,27 @@ def main():
         ere_grep()
     elif sys.argv[1] == "ere_s":
         ere_grep_single_match()
-    elif sys.argv[1] == "pcre":
-        pcre_grep()
-    elif sys.argv[1] == "pcre_s":
-        pcre_grep_single_match()
+    elif sys.argv[1] == "turbo":
+        turbo_ere_grep()
+    elif sys.argv[1] == "memory":
+        mem_ere_grep()
+    elif sys.argv[1] == "time":
+        time_ere_grep()
     else:
         print("incorrect usage, arg 1 should be either 'ere' or 'pcre'")
 
 
 # this function reads in a list of regexes performs egrep using each regex
 def ere_grep():
-    with open(sys.argv[3], 'r') as rf, open('results/results.txt', 'w') as results:
-        for line in rf:
-            regex = line.strip()
-            try:
-                output = subprocess.check_output(['egrep', regex, sys.argv[2]], text=True)
-                results.write(output)
-            except subprocess.CalledProcessError as e:
-                if e.returncode == 1:
-                    results.write("no matches found\n") # note, this is similar to bkase's grep output
-                else:
-                    results.write(f"error: {e} \n")
-            results.write("\n")
+    with open('results/results.txt', 'w') as results:
+        try:
+            output = subprocess.check_output(['egrep', '-f', sys.argv[3], sys.argv[2]], text=True)
+            results.write(output)
+        except subprocess.CalledProcessError as e:
+            if e.returncode == 1:
+                results.write("no matches found\n") # note, this is similar to bkase's grep output
+            else:
+                results.write(f"error: {e} \n")
 
 # this function takes in a string to perform egrep onto an input file
 def ere_grep_single_match():
@@ -50,11 +51,48 @@ def ere_grep_single_match():
             else:
                 results.write(f"error: {e} \n")
 
-def pcre_grep():
+def turbo_ere_grep():
+
     pass
 
-def pcre_grep_single_match():
-    pass
+def mem_ere_grep():
+    command = ["python3"] + sys.argv
+    print(command)
+    # psutil.Popen(["python3"])
+
+    high = 0
+    with open(sys.argv[3], 'r') as rf, open('results/results.txt', 'w') as results:
+        for line in rf:
+            regex = line.strip()
+
+            current = psutil.Popen(['egrep', regex, sys.argv[2]])
+            while current.is_running():
+                try:
+                    h = current.memory_info().rss
+                    high = max(high, h)
+                except:
+                    break
+            results.write("\n")
+
+    print(f"Highest memory: {high}")
+    print(f"Highest memory util: {high / psutil.virtual_memory().total}")
+
+def time_ere_grep():
+    start_time = time.time()
+    try:
+        output = subprocess.check_output(['egrep', '-f', sys.argv[3], sys.argv[2]], text=True)
+        # print(output)
+    except subprocess.CalledProcessError as e:
+        if e.returncode == 1:
+            print("no matches found\n") # note, this is similar to bkase's grep output
+        else:
+            print(f"error: {e} \n")
+    end_time = time.time()
+
+    
+    # results.write(f"Start time: {start_time}\n")
+    # results.write(f"End time: {end_time}\n")
+    print(f"Elapsed time: {end_time - start_time}\n")
 
 if __name__ == "__main__":
     main()
